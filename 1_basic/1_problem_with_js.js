228865eb2da9c592babf3ff66ea59e26f2cce304
younha00 <ayeon0@office.skhu.ac.kr>
2024-01-28 18:34:26 +0900

    2_basics

--- 1_basic/1_problem_with_js.js
+++ 1_basic/1_problem_with_js.js
@@ -1,14 +1,6 @@
-/**
- * JS의 문제점
- * 
- * 
- * dynamicall typed
- */
-
-
 "use strict";
 function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(1, 2));
-console.log(add(1, '2'));
+// console.log(add(1, '2'));
---
+++ 1_basic/2_basics.js
@@ -0,0 +1,47 @@
+"use strict";
+/**
+ * Types
+ */
+let helloText = 'Hello';
+// helloText = true; // Error: Type 'boolean' is not assignable to type 'string'.
+/**
+ * JS의 7개의 타입
+ * 7개의 타입
+ */
+const stringVar = 'String';
+const numberVar = 10;
+const booleanVar = true;
+const bigIntVar = BigInt(999);
+const symbolVar = Symbol(1);
+const nullVar = null;
+const undefinedVar = undefined;
+/**
+ * TS에만 존재하는 타입
+ */
+// any - 아무 타입이나 입력할 수 있는 치트키 같은 타입
+let anyVar;
+anyVar = 'String';
+anyVar = 10;
+anyVar = true;
+let testNumber = anyVar; // anyVar가 number라고 타입을 지정해도 에러가 나지 않음
+let testString = anyVar;
+let testBoolean = anyVar;
+// unknown - 알 수 없는 타입
+let unknownType;
+unknownType = 'String';
+unknownType = 10;
+unknownType = true;
+// let testNumber2: number = unknownType; // 다른 변수에 할당 불가
+// let testString2: string = unknownType;
+// let testBoolean2: boolean = unknownType;
+let unknownType2 = unknownType; // unknown 타입에 unknown 타입을 할당 가능
+let anyType2 = unknownType; // unknown 타입에 any 타입을 할당 가능
+// never - 어떠한 값도 저장되거나 반환되지 않을 떄 사용하는 타입
+// let neverType: never = null;
+// let neverType: never = undefined;
+// let neverType: never = 'String';
+/**
+ * 리스트 타입
+ */
+const koreanGirlGroup = ['아이브', '레드벨벳', '블랙핑크'];
+const booleanList = [true, false, true];
